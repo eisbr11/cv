50b82e3ee009aa6066677ecc170ec08070b035e0
Felix Schliemann <felix.schliemann@diva-e.com>
2021-02-06 23:54:59 +0100

    feat: center the education content

--- components/Education/Education.component.tsx
+++ components/Education/Education.component.tsx
@@ -1,20 +1,24 @@
 import { Container, Grid, Typography } from '@material-ui/core';
 
+import useStyles from './Education.styles';
 import EducationTimeline from './component/EducationTimeline.component';
 
-const EducationComponent = () => (
-  <Container>
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Typography variant="h3">
-          Education
-        </Typography>
+const EducationComponent = () => {
+  const classes = useStyles();
+  return (
+    <Container className={classes.container}>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h4" align="center">
+            My educational background
+          </Typography>
+        </Grid>
+        <Grid item xs={12}>
+          <EducationTimeline />
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <EducationTimeline />
-      </Grid>
-    </Grid>
-  </Container>
-);
+    </Container>
+  );
+};
 
 export default EducationComponent;
