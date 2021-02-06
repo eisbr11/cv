22d2c81d72db6e3f7f32a70a4db0ada72a0e92e1
Felix Schliemann <felix.schliemann@diva-e.com>
2021-02-06 23:41:03 +0100

    feat: added age to welcome page

--- components/Welcome/Welcome.component.tsx
+++ components/Welcome/Welcome.component.tsx
@@ -2,6 +2,9 @@ import { Typography } from '@material-ui/core';
 import Image from 'next/image';
 
 import useStyles from '@components/Welcome/Welcome.styles';
+import moment from 'moment';
+
+const age = moment().diff('1989-02-12', 'years');
 
 const Welcome = () => {
   const classes = useStyles();
@@ -25,7 +28,7 @@ const Welcome = () => {
             Senior Frontend Developer from Berlin
           </Typography>
           <Typography variant="body2">
-            Hey there, I’m Felix. I work as a Senior Frontend Developer in Berlin.
+            {`Hey there, I am Felix. I am ${age} years old and work as a Senior Frontend Developer in Berlin.`}
           </Typography>
         </div>
       </div>
