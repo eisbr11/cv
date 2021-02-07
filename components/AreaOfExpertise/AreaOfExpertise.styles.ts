7c2e4bb642fc819bd5f6c725fb49ca366e8965c1
Felix Schliemann <felix.schliemann@diva-e.com>
2021-02-08 00:20:43 +0100

    fix: fixed styling issue

--- components/AreaOfExpertise/AreaOfExpertise.styles.ts
+++ components/AreaOfExpertise/AreaOfExpertise.styles.ts
@@ -2,7 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   container: {
-    margin: theme.spacing(3, 0),
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
     display: 'flex',
     alignSelf: 'center',
   },
